ef8bc1df6df7f89bd9c4f15505dd7847faddf542
cruwmorgan <morganokeke@gmail.com>
2023-06-02 00:16:09 -0700

    Select some Amenities to be comfortable!

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.amenities.popover ul li input[type=checkbox]').on('change', function () {
+    let dict = [];
+    const myId = [];
+    let jCheck = $(this).is(':checked');
+    if (jCheck) {
+      $('input[type=checkbox]:checked').each(function () {
+        dict.push($(this).attr('data-name'));
+        myID.push($(this).attr('data-id'));
+      });
+    } else {
+      $('input[type=checkbox]:not(:checked)').each(function () {
+        dict.pop($(this).attr('data-name'));
+        myID.pop($(this).attr('data-id'));
+      });
+    }
+    if (dict.length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(dict.join(', '));
+    }       
+    console.log(myId);
+  });
+}); 
