01f0c452db0bb91bd79c51d1bc34a7f5790c13c5
cruwmorgan <morganokeke@gmail.com>
2023-06-02 01:13:13 -0700

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $('.amenities.popover ul li input[type=checkbox]').on('change', function () {
+    let dict = [];
+    const myId = [];
+    let jCheck = $(this).is(':checked');
+    if (jCheck) {
+      $('input[type=checkbox]:checked').each(function () {
+        dict.push($(this).attr('data-name'));
+        myID.push($(this).attr('data-id'));
+      });
+    } else {
+      $('input[type=checkbox]:not(:checked)').each(function () {
+        dict.pop($(this).attr('data-name'));
+        myID.pop($(this).attr('data-id'));
+      });
+    }
+    if (dict.length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(dict.join(', '));
+    }       
+    console.log(myId);
+  });
+  const apiUrl = 'http://0.0.0.0:5001/api/v1/status/';
+  $.ajax({
+    url: apiUrl,
+    type: 'GET',
+    method: 'GET',
+    dataType: 'json',
+    success: function(response){
+      $('div#api_status').addClass('available');
+    },
+    error: function(){
+      $('div#api_status').removeClass('available');
+    }
+  });
+}); 
